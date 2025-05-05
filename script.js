d1ef6cf1fb5b369a1a9bebb2c99a923281c787a8
ThereseGlode <therese.glodedata@gmail.com>
2025-05-05 12:56:05 +0200

    Burger

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hello!');
+// Hent elementene
+const burgerIcon = document.getElementById('burger-icon');
+const headerNav = document.getElementById('headerNav');
+
+// Legg til en event listener for å vise/skjule menyen
+burgerIcon.addEventListener('click', () => {
+  // Legger til eller fjerner 'show' klassen på navigasjonen
+  headerNav.classList.toggle('show');
+});
